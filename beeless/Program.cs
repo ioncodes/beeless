7791f03a5b433b4da34e309eef21af9e71402ccc
ion <ionix@protonmail.com>
2017-01-19 16:25:19 +0100

    init

--- beeless/Program.cs
+++ beeless/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
 
 namespace beeless
 {
@@ -10,6 +13,50 @@ namespace beeless
     {
         static void Main(string[] args)
         {
+            ModuleDefMD module = ModuleDefMD.Load(args[0]);
+            int fields = 0;
+            int properties = 0;
+            foreach (var type in module.Types)
+            {
+                var methods = 0;
+                foreach (var method in type.Methods)
+                {
+                    if (method.Name.Length > 10)
+                    {
+                        method.Name = "method_" + methods;
+                        methods++;
+                    }
+
+                    var paramaters = 0;
+                    foreach (var paramater in method.Parameters)
+                    {
+                        if (paramater.Name.Length > 10)
+                        {
+                            paramater.Name = "param_" + paramaters;
+                            paramaters++;
+                        }
+                    }
+                }
+
+                foreach (var field in type.Fields)
+                {
+                    if (field.Name.Length > 10)
+                    {
+                        field.Name = "field_" + fields;
+                        fields++;
+                    }
+                }
+
+                foreach (var property in type.Properties)
+                {
+                    if (property.Name.Length > 10)
+                    {
+                        property.Name = "property_" + properties;
+                        properties++;
+                    }
+                }
+            }
+            module.Write(Path.GetFileNameWithoutExtension(args[0]) + "_cleaned" + Path.GetExtension(args[0]));
         }
     }
 }
